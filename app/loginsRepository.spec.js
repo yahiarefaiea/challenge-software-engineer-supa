02dfb8bc1206b7b19e944b3d8a462a8007f05ed3
Yahia Refaiea <yahiarefaiea@gmail.com>
2023-02-09 06:24:44 +0000

    improve tests

--- app/loginsRepository.spec.js
+++ app/loginsRepository.spec.js
@@ -9,27 +9,19 @@ import {
   updateLoginsArray
 } from './loginsRepository'
 
-const loginsRecords = ["2023-04-02 12:48:18", "asdasd", "2023-03-16 11:48:18", "asdasd", "asdasd", "2023-04-04 03:48:18", "asdasd", "2023-03-17 02:48:18", "asdasd", "asdasd", "2023-03-19 20:48:18"]
+const dirtyRecords = ["2023-04-02 12:48:18", "asdasd", "2023-03-16 11:48:18", "asdasd", "asdasd", "2023-04-04 03:48:18", "asdasd", "2023-03-17 02:48:18", "asdasd", "asdasd", "2023-03-19 20:48:18"]
+const expectedResult = [{ START: '2023-03-16', END: '2023-03-17', LENGTH: 2 }, { START: '2023-03-19', END: '2023-03-19', LENGTH: 1 }, { START: '2023-04-02', END: '2023-04-02', LENGTH: 1 }, { START: '2023-04-04', END: '2023-04-04', LENGTH: 1 }]
+const unorderedResult = [{ START: '2023-04-04', END: '2023-04-04', LENGTH: 1 }, { START: '2023-04-02', END: '2023-04-02', LENGTH: 1 }, { START: '2023-03-19', END: '2023-03-19', LENGTH: 1 }, { START: '2023-03-16', END: '2023-03-17', LENGTH: 2 }]
 
 describe('Output is valid only if it...', function() {
   it('doesn\'t contain non-dates values', function() {
     expect(
-      getLoginsByStreaks(loginsRecords)
-    ).to.deep.match([
-      { START: '2023-03-16', END: '2023-03-17', LENGTH: 2 },
-      { START: '2023-03-19', END: '2023-03-19', LENGTH: 1 },
-      { START: '2023-04-02', END: '2023-04-02', LENGTH: 1 },
-      { START: '2023-04-04', END: '2023-04-04', LENGTH: 1 }
-    ])
+      getLoginsByStreaks(dirtyRecords)
+    ).to.deep.match(expectedResult)
   })
   it('is sorted by descending length', function() {
     expect(
-      getLoginsByStreaks(loginsRecords)
-    ).to.not.deep.match([
-      { START: '2023-04-04', END: '2023-04-04', LENGTH: 1 },
-      { START: '2023-04-02', END: '2023-04-02', LENGTH: 1 },
-      { START: '2023-03-19', END: '2023-03-19', LENGTH: 1 },
-      { START: '2023-03-16', END: '2023-03-17', LENGTH: 2 }
-    ])
+      getLoginsByStreaks(dirtyRecords)
+    ).to.not.deep.match(unorderedResult)
   })
 })
--- app/utilities.spec.js
+++ app/utilities.spec.js
@@ -1,34 +1,37 @@
 import {expect} from 'chai'
 import {isValidDate, formatDate} from './utilities'
 
+const dateSampleAsString = '2021-03-13 15:13:05'
+const dateSampleAsDateObject = new Date()
+const dateSampleAsISO = '2021-03-13T20:24:31.371Z'
+const dirtyDateSampleContainsDate = 'foo-bar-2021-03-13 15:13:05'
+const randomString = 'foo-bar'
+const emptyString = ''
+const dateFormated = '2021-03-13'
+
 describe('Date is valid if it is...', function() {
   it('formated as YYYY-MM-DD hh:mm:ss', function() {
-    expect(isValidDate(
-      '2021-03-13 15:13:05'
-    )).to.equal(true)
+    expect(isValidDate(dateSampleAsString)).to.equal(true)
   })
   it('formated as known value date, like `new Date()`', function() {
-    expect(isValidDate(
-      new Date()
-    )).to.equal(true)
+    expect(isValidDate(dateSampleAsDateObject)).to.equal(true)
+  })
+  it('not a dirty date', function() {
+    expect(isValidDate(dirtyDateSampleContainsDate)).to.equal(false)
   })
   it('not a random string', function() {
-    expect(isValidDate('foo-bar')).to.equal(false)
+    expect(isValidDate(randomString)).to.equal(false)
   })
   it('not an empty string', function() {
-    expect(isValidDate('')).to.equal(false)
+    expect(isValidDate(emptyString)).to.equal(false)
   })
 })
 
 describe('As per requirements, the date\'s format is valid only if it...', function() {
   it('matches the `YYYY-MM-DD` format', function() {
-    expect(formatDate(
-      '2021-03-13 15:13:05'
-    )).to.equal('2021-03-13')
+    expect(formatDate(dateSampleAsString)).to.equal(dateFormated)
   })
   it('doesn\'t match any other format', function() {
-    expect(formatDate(
-      '2021-03-13T20:24:31.371Z'
-    )).to.equal('2021-03-13')
+    expect(formatDate(dateSampleAsISO)).to.equal(dateFormated)
   })
 })
